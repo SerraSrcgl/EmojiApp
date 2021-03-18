a285d4d470e73ea2b36bfedf316ad10fe7d54eae
SerraSrcgl <raclawicka123@gmail.com>
2021-03-18 13:47:01 +0300

    riddles added

---
+++ EmojiApp/Riddle.swift
@@ -0,0 +1,8 @@
+//
+//  Riddle.swift
+//  EmojiApp
+//
+//  Created by Serra Saracoglu on 18/03/2021.
+//
+
+import Foundation
--- EmojiApp/ViewController.swift
+++ EmojiApp/ViewController.swift
@@ -13,7 +13,47 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func avacadoButton(_ sender: UIButton) {
+        print("avasoda")
+        let alert=UIAlertController(title: "Title", message: "This is a pear-shaped fruit with a rough leathery skin, smooth oily edible flesh, and a large pit in the center it is most used in guacamole dip", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "I don't like it", style: .cancel, handler: {
+            action in
+            print("tapped dismiss")
+        }))
+        present(alert, animated: true)
+    }
+    
+    @IBAction func moonButton(_ sender: UIButton) {
+        let alert=UIAlertController(title: "Title", message: "i am something in the sky That seems to shine at night However, I’m not a star I am Earth’s satellite", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "I don't like it", style: .cancel, handler: {
+            action in
+            print("tapped dismiss")
+        }))
+        present(alert, animated: true)
+        print("moon")
+        
+    }
+    @IBAction func monkeyButton(_ sender: UIButton) {
+        let alert=UIAlertController(title: "Title", message: "I Live in the jungle Up in trees I swing There’s one called Rafiki In The Lion King", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "I don't like it", style: .cancel, handler: {
+            action in
+            print("tapped dismiss")
+        }))
+        present(alert, animated: true)
+        print("monkey")
+    }
+    
+    @IBAction func flowerButton(_ sender: UIButton) {
+        let alert=UIAlertController(title: "Title", message: "This is a type of plant Which sounds like something in the sky It is very yello And can grow to be ten feet high", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "I don't like it", style: .cancel, handler: {
+            action in
+            print("tapped dismiss")
+        }))
+        present(alert, animated: true)
+        print("flower")
+    }
+    func showAlert(){
+        
+    }
 }
 
